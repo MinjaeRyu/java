4e5836bdbf6cc9d71486695e292d939d9a441801
Minjae Ryu <76580018+MinjaeRyu@users.noreply.github.com>
2025-09-01 11:01:04 +0900

    Update VariableScopeExample.java

--- chap02/src/sec01/exam04/VariableScopeExample.java
+++ chap02/src/sec01/exam04/VariableScopeExample.java
@@ -2,14 +2,14 @@ package sec01.exam04;
 
 public class VariableScopeExample {
 	public static void main(String[] args) {
-//		int v1 = 15;
-//		int v2 = 25;
-//		
-//		if (v1 > 10) {
-//			v2 = v1 - 10;
-//		}
-//		
-//		int v3 = v1 + v2 + 5;
+		int v1 = 15;
+		int v2 = 25;
+		
+		if (v1 > 10) {
+			v2 = v1 - 10;
+		}
+		
+		int v3 = v1 + v2 + 5;
 
 	}
 }
